7dba299d53f630b1d53b008d4c584c5a7d8190ad
7trail <72806227+7trail@users.noreply.github.com>
2022-08-15 14:23:42 -0500

    Added the completed template mod.

---
+++ WoLMod/NewDamageUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOLModTemplate
+{
+	public class NewDamageUp : Item
+	{
+		public NewDamageUp()
+		{
+			this.ID = NewDamageUp.staticID;
+			this.category = Item.Category.Offense;
+			this.damageMod = new NumVarStatMod(this.ID, 1.0f, 10, VarStatModType.Multiplicative, false); //Doubles damage
+		}
+		public override string ExtraInfo
+		{
+			get
+			{
+				return base.PercentToStr(this.damageMod, "+");
+			}
+		}
+
+		public override void Activate()
+		{
+			this.SetModStatus(true);
+		}
+		public override void Deactivate()
+		{
+			this.SetModStatus(false);
+		}
+
+		public virtual void SetModStatus(bool givenStatus)
+		{
+			StatManager.ModifyAllStatData(this.damageMod, this.parentSkillCategory, StatData.damageStr, new StatManager.ModApplyConditional(base.IgnoreStatusConditional), givenStatus);
+		}
+		public static string staticID = "ModName::NewDamageUp";
+
+		protected NumVarStatMod damageMod;
+	}
+
+}
---
+++ WoLMod/WoLModPlugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using LegendAPI;
+namespace WOLModTemplate {
+    [BepInPlugin("com.NewDeveloper.WolModTemplate", "WoLModTemplate", "0.1.0")]
+    public class WoLModPlugin : BaseUnityPlugin {
+
+        void Awake() {
+            OutfitInfo outfit = new OutfitInfo();
+            outfit.name = "Cool Guy";
+            outfit.outfit = new Outfit("ModName::OutfitName", 1, new List<OutfitModStat>()
+            {
+                new OutfitModStat(OutfitModStat.OutfitModType.Health,100,0,0,false),
+                new OutfitModStat(OutfitModStat.OutfitModType.Speed,0,0.2f,0,false)
+            }, false, false);
+
+            Outfits.Register(outfit);
+
+            ItemInfo item = new ItemInfo();
+            item.name = NewDamageUp.staticID;
+            item.text = new TextManager.ItemInfo()
+            {
+                itemID = NewDamageUp.staticID,
+                displayName = "Super Necklace",
+                description = "Increases damage greatly!"
+            };
+            item.tier = 1;
+            item.priceMultiplier = 1;
+            item.icon = ImageHandler.LoadSprite("Super Necklace");
+            item.item = new NewDamageUp();
+            Items.Register(item);
+        }
+    }
+}
